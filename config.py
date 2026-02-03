00708b37d0a3ef08ec0902f54677d84270ea7362
mac <mac@Lucids-MacBook-Pro.local>
2026-02-03 20:27:22 +0100

    fix: include config.py for vercel deployment

---
+++ config.py
@@ -0,0 +1,4 @@
+token = "8467457156:AAENyoV-Nw8gStkb8KIKfjcTapcE_OFRaWI"  # telegram token
+logs = None  # logs channel id, if none set to None
+max_filesize = 50000000  # bytes
+output_folder="/tmp/yt-dlp-telegram"
